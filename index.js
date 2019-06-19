6d6c11d87acb2ffd49dd4197699177a6dd0a1264
Kevin Skilton <kevin.skilton@powerhrg.com>
2019-06-19 16:07:25 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solution in this file!
+const logDriverNames = function(drivers){
+  drivers.forEach(function (driver){
+    console.log(driver.name)
+  })
+}
+
+const logDriversByHometown = function (drivers, hometown){
+  drivers.forEach(function (driver){
+    if (driver.hometown === hometown){
+      console.log(driver.name)
+    }
+  })
+}
+
+const driversByRevenue = function (drivers, revenue) {
+  return drivers.slice().sort(function (driverOne, driverTwo){
+    return driverOne.revenue - driverTwo.revenue
+  })
+}
+
+const driversByName = function (drivers, name){
+  return drivers.slice().sort(function(driverOne, driverTwo){
+    return driverOne.name.localeCompare(driverTwo.name)
+  })
+}
+
+const totalRevenue = function(drivers){
+  let total = 0
+
+  drivers.forEach(function(driver){
+    total += driver.revenue
+  })
+  return total
+}
+
+const averageRevenue = function(drivers){
+  return totalRevenue(drivers) / drivers.length
+}
